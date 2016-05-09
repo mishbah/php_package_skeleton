d47de1b74bd106564db465985c52396512727df0
misbah <misbah.qadri@gmail.com>
2016-05-10 01:53:35 +0700

    Added DiffbotTest.php

---
+++ tests/DiffbotTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Mishbah\Diffbot\Test;
+
+use Mishbah\Diffbot\Diffbot;
+
+/**
+ * @runTestsInSeparateProcesses
+ */
+class DiffbotTest extends \PHPUnit_Framework_TestCase
+{
+    public function invalidTokens()
+    {
+        return [
+            'empty'        => [ '' ],
+            'a'            => [ 'a' ],
+            'ab'           => [ 'ab' ],
+            'abc'          => [ 'abc' ],
+            'digit'        => [ 1 ],
+            'double-digit' => [ 12 ],
+            'triple-digit' => [ 123 ],
+            'bool'         => [ true ],
+            'array'        => [ ['token'] ],
+        ];
+    }
+
+    public function validTokens()
+    {
+        return [
+            'token'      => [ 'token' ],
+            'short-hash' => [ '123456789' ],
+            'full-hash'  => [ 'akrwejhtn983z420qrzc8397r4' ],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidTokens
+     */
+    public function testSetTokenRaisesExceptionOnInvalidToken($token)
+    {
+        $this->setExpectedException('InvalidArgumentException');
+        Diffbot::setToken($token);
+    }
+
+
+    /**
+     * @dataProvider validTokens
+     */
+    public function testSetTokenSucceedsOnValidToken($token)
+    {
+        Diffbot::setToken($token);
+        $bot = new Diffbot();
+        $this->assertInstanceOf('\Mishbah\Diffbot\Diffbot', $bot);
+    }
+
+    public function testInstantiationWithNoGlobalTokenAndNoArgumentRaisesAnException()
+    {
+        $this->setExpectedException('\Mishbah\Diffbot\Exceptions\DiffbotException');
+        new Diffbot();
+    }
+
+    public function testInstantiationWithGlobalTokenAndNoArgumentSucceeds()
+    {
+        Diffbot::setToken('token');
+        $bot = new Diffbot();
+        $this->assertInstanceOf('Mishbah\Diffbot\Diffbot', $bot);
+    }
+
+    public function testInstantiationWithNoGlobalTokenButWithArgumentSucceeds()
+    {
+        $bot = new Diffbot('token');
+        $this->assertInstanceOf('Mishbah\Diffbot\Diffbot', $bot);
+    }
+}
