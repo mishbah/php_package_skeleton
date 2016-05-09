0184965b32b9da8bd9bfa82731ecc55fbcce0970
misbah <misbah.qadri@gmail.com>
2016-05-09 21:27:47 +0700

    Added Diffbot

---
+++ src/Diffbot.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Mishbah\Diffbot;
+
+use Mishbah\Diffbot\Exceptions\DiffbotException;
+
+/**
+ * Class Diffbot
+ *
+ * The main class for API consumption
+ *
+ * @package Swader\Diffbot
+ */
+class Diffbot
+{
+    /** @var string The API access token */
+    protected static $token = null;
+
+    /** @var string The instance token, settable once per new instance */
+    protected $instanceToken;
+
+    /**
+     * @param string|null $token The API access token, as obtained on diffbot.com/dev
+     * @throws DiffbotException When no token is provided
+     */
+    public function __construct($token = null)
+    {
+        if ($token === null) {
+            if (self::$token === null) {
+                $msg = 'No token provided, and none is globally set. ';
+                $msg .= 'Use Diffbot::setToken, or instantiate the Diffbot class with a $token parameter.';
+                throw new DiffbotException($msg);
+            }
+        } else {
+            self::validateToken($token);
+            $this->instanceToken = $token;
+        }
+    }
+
+    /**
+     * Sets the token for all future new instances
+     * @param $token string The API access token, as obtained on diffbot.com/dev
+     * @return void
+     */
+    public static function setToken($token)
+    {
+        self::validateToken($token);
+        self::$token = $token;
+    }
+
+    protected static function validateToken($token)
+    {
+        if (!is_string($token)) {
+            throw new \InvalidArgumentException('Token is not a string.');
+        }
+        if (strlen($token) < 4) {
+            throw new \InvalidArgumentException('Token "' . $token . '" is too short, and thus invalid.');
+        }
+        return true;
+    }
+}
