b466bfa55d5ce46491894a55282770b823d6eaa7
misbah <misbah.qadri@gmail.com>
2016-05-10 02:40:55 +0700

    Added ApiTest

---
+++ tests/Abstracts/ApiTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Mishbah\Diffbot\Test;
+
+use Mishbah\Diffbot\Abstracts\Api;
+
+class ApiTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function buildMock()
+    {
+        return $this->getMockForAbstractClass('Mishbah\Diffbot\Abstracts\Api');
+    }
+
+    public function validTimeouts()
+    {
+        return [
+            'zero' => [0],
+            '1000' => [1000],
+            '2000' => [2000],
+            '3000' => [3000],
+            '3 mil' => [3000000],
+            '40 mil' => [40000000]
+        ];
+    }
+
+    public function invalidTimeouts()
+    {
+        return [
+            'negative_big' => [-298979879827],
+            'negative_small' => [-4983],
+            'string ' => ['abcef'],
+            'empty string' => [''],
+            'bool' => [false]
+        ];
+    }
+
+    public function testSetEmptyTimeoutSuccess()
+    {
+        /** @var Api $mock */
+        $mock = $this->buildMock();
+        $mock->setTimeout();
+    }
+
+    /**
+     * @dataProvider invalidTimeouts
+     */
+    public function testSetTimeoutInvalid($timeout)
+    {
+        /** @var Api $mock */
+        $mock = $this->buildMock();
+        $this->setExpectedException('InvalidArgumentException');
+        $mock->setTimeout($timeout);
+    }
+
+    /**
+     * @dataProvider validTimeouts
+     */
+    public function testSetTimeoutValid($timeout)
+    {
+        /** @var Api $mock */
+        $mock = $this->buildMock();
+        $mock->setTimeout($timeout);
+    }
+}
